c45a550450fe26713b914fe42bd22110f9b4efa6
LeandroParisi <lepcbelisario@gmail.com>
2020-10-13 12:52:53 -0300

    Updates

---
+++ precache-manifest.0bae128186e5d3086b07b04ad72aa7cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcc88e02ec7bded4ac874172bad6a0c",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "499fc6c50cd95abba97a",
+    "url": "/pokedex/static/css/main.9de2557b.chunk.css"
+  },
+  {
+    "revision": "cad414317123bc2ce0fa",
+    "url": "/pokedex/static/js/2.006c0372.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.006c0372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499fc6c50cd95abba97a",
+    "url": "/pokedex/static/js/main.aa0b2fac.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "72fb9000eae1c7fceb0a9bfd34ef7994",
+    "url": "/pokedex/static/media/Pokedex.72fb9000.webp"
+  },
+  {
+    "revision": "8e7d2c13d299ea92e5cb10e8af05eaf7",
+    "url": "/pokedex/static/media/loading-page.8e7d2c13.gif"
+  },
+  {
+    "revision": "ea81a3942d82f1ac442c40cd06b0edee",
+    "url": "/pokedex/static/media/open-pokeball.ea81a394.png"
+  },
+  {
+    "revision": "b7b543c3d84d657e2b38761484ee9cd2",
+    "url": "/pokedex/static/media/page404.b7b543c3.webp"
+  },
+  {
+    "revision": "0b9c06b622e97be8dac442a711672717",
+    "url": "/pokedex/static/media/pokedex-bg-15.0b9c06b6.jpg"
+  },
+  {
+    "revision": "ffd4173c82b31b140f2428f7a5dc5c28",
+    "url": "/pokedex/static/media/star3.ffd4173c.gif"
+  }
+]);
