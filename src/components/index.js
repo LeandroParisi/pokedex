026a28388fdccfd5a042ebab587e79d3ed9c89eb
LeandroParisi <lepcbelisario@gmail.com>
2020-10-13 09:57:20 -0300

    Re Starting project import

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Button } from './Button';
-export { default as Header } from './Header';
-export { default as Pokedex } from './Pokedex';
-export { default as Pokemon } from './Pokemon';
-export { default as PokemonDetails } from './PokemonDetails';
-export { default as FavoritePokemons } from './FavoritePokemons'
-export { default as CreatePokemonForm } from './CreatePokemonForm'
-export { default as DeletePokemonForm } from './DeletePokemonForm'
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as About } from './About';
-export { default as NotFound } from './NotFound';
-export { default as CrudPage } from './CrudPage';
-export { default as Loading } from './Loading';
-
